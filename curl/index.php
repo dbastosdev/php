0e84ba93535340270b069e82383f42420b18fb08
dbastosdev <dbastos.dev@gmail.com>
2022-06-01 22:05:26 -0300

    trabalhando com curl no php

---
+++ curl/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Tem a função de fazer a integração com serviços externos
+
+// Iniciando a biblioteca curl
+$ch = curl_init();
+// Configurando: biblioteca, configuração de url, endereço url 
+curl_setopt($ch, CURLOPT_URL, "https://api.chucknorris.io/jokes/categories");
+
+// Pegando o resultado da consulta
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Executando a requisição
+$resposta = curl_exec($ch);
+// fechando a conexão
+curl_close($ch);
+
+// Exibindo os dados
+print_r($resposta);
+
+
+?>
