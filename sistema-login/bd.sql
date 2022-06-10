ea22846be04a0e1d0b33d4fe313fae74c2fd0628
dbastosdev <dbastos.dev@gmail.com>
2022-06-09 21:12:58 -0300

    sistema de login simples

---
+++ sistema-login/bd.sql
@@ -0,0 +1,6 @@
+/*
+Inserindo coluna de senha no banco de dados de usuarios
+*/ 
+
+ALTER TABLE usuario ADD senha varchar(150); 
+
---
+++ sistema-login/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Inicia sessão. Sempre a primeira declaração da página 
+session_start();
+
+// Verificação simples de login. Testa para avaliar se a sessão está com um login de usuário válido
+if(isset($_SESSION['id']) && empty($_SESSION['id']) == false) {
+    echo "Área restrita";
+    // Caso não esteja com a sessão setada, redireciona para login
+} else {
+    header('Location: login.php');
+    exit;
+}
