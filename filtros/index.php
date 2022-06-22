d3c17e22909224366eeeb2135c89a4fb02c2e64f
dbastosdev <dbastos.dev@gmail.com>
2022-06-22 18:38:17 -0300

    filtro finalizado

--- filtros/index.php
+++ filtros/index.php
@@ -4,8 +4,22 @@
 require_once 'config.php';
 
 // Buscando dados no mysql
-$sql = $pdo->query("SELECT * FROM user_filter");
-$sql = $sql->fetchAll();
+$sexo; // tornando a variável global
+
+// Tratando selecção do filtro de sexo
+//if($_POST['sexo'] == 0 || $_POST['sexo'] == 1 ) {
+if(isset($_POST['sexo']) && $_POST['sexo'] < 2) { // Já é um valor fora do nosso range
+    $sexo = $_POST['sexo'];
+    $sql = $pdo->prepare("SELECT * FROM user_filter WHERE sexo = :sexo");
+    $sql->bindValue(':sexo', $sexo);
+    $sql->execute();
+    $sql = $sql->fetchAll();
+} else {
+    $sexo = ''; // Caso não esteja setado $_POST['sexo']. Do contrário buga a lógica.
+    $sql = $pdo->query("SELECT * FROM user_filter");
+    $sql = $sql->fetchAll();
+}
+
 
 // Conversor de números e sexo
 // Com isso, utilizamos os números como chave para fazer a conversão entre masculino e feminino
@@ -17,8 +31,20 @@ $sexos = array(
 ?>
 
 
-<!-- Criando um formulário para o filtro --> 
+<!-- Criando um formulário para o filtro 
 
+Ao realizar a selecção, a parte do arquivo correspondente ao PHP poderá receber a escolha e 
+utilizar dentro do SELECT para fitrar adequadamente a opção
+
+--> 
+<form method="POST">
+    <select name="sexo">
+        <option value="0" <?php echo ($sexo == '0')? 'selected' : ''; ?>>Masculino</option> <!-- Forçando a marcação da opção com o selected -->
+        <option value="1" <?php echo ($sexo == '1')? 'selected' : ''; ?>>Feminino</option>
+        <option value="2" <?php echo ($sexo > 1 || $sexo == '')? 'selected' : ''; ?>>---</option>
+    </select>
+    <input type="submit" value="filtrar">
+</form>
 
 
 <table border="1">
